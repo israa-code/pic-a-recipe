45e29c0b2ae33efb8b179f5f2f50cfb8f33f18f2
Izzy-100 <israasakhakhni@gmail.com>
2026-02-06 11:54:35 -0800

    feat(ui): add loading screen component with animated loader

---
+++ pic-a-recipe/src/components/Loading/Loading.jsx
@@ -0,0 +1,16 @@
+import './Loading.css';
+
+const Loading = () => {
+    
+    return (
+        <body id='loading-page'>
+            <div>
+                <div class="loader"></div>
+                <h3>Loading...</h3>
+            </div>
+        </body>
+    );
+}
+
+export default Loading;
+
