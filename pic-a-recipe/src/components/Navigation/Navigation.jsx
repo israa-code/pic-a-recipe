b7e6461214d61689ada6fcf63802e3fed2e6a85b
Izzy-100 <israasakhakhni@gmail.com>
2026-02-06 11:57:50 -0800

    feat(ui): display loading screen in Navigation and add Outlet for nested routes

--- pic-a-recipe/src/components/Navigation/Navigation.jsx
+++ pic-a-recipe/src/components/Navigation/Navigation.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import {Outlet} from 'react-router-dom';
+import Loading from '../Loading/Loading.jsx';
 
-const Navigation = () => {
+const Navigation = ({isLoading}) => {
+    if(isLoading){
+        return <Loading/>
+    }
     return (
-        <nav style={{display:'flex', justifyContent:'flex-end', overflow:'hidden'}}>
-            <div className='br--left br4 shadow-5 bg-dark-green mt2 grow'>
-                <p className='f3 washed-green link dim pa3 ma0 pointer'>Sign Out</p>
-            </div>
-        </nav>
+        <>
+            <nav style={{display:'flex', justifyContent:'flex-end', overflow:'hidden'}}>
+                <div className='br--left br4 shadow-5 bg-dark-green mt2 grow'>
+                    <p className='f3 washed-green link dim pa3 ma0 pointer'>Sign Out</p>
+                </div>
+            </nav>
+            <Outlet />
+        </>
     );
 }
 
